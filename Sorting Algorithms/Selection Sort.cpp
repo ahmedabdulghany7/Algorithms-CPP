3ed084d7fd332572c6cacf26de84c8713eb8ff7b
Ahmed Abd Elghani <30578096+ahmedabdulghany7@users.noreply.github.com>
2024-08-14 20:43:31 +0300

    Update Selection Sort.cpp

--- Sorting Algorithms/Selection Sort.cpp
+++ Sorting Algorithms/Selection Sort.cpp
@@ -2,10 +2,10 @@
 // Space Complexity: O(n)
 
 void selection_sort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i < n - 1; i++) {
         int min_index = i;
-        for(int j = i+1;j<n;j++) {
-            if(arr[min_index]>arr[j]) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[min_index] > arr[j]) {
                 min_index = j;
             }
         }
@@ -29,10 +29,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i < n - 1; i++) {
         int min_index = i;
-        for(int j = i+1;j<n;j++) {
-            if(arr[min_index]>arr[j]) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[min_index] > arr[j]) {
                 min_index = j;
             }
         }
