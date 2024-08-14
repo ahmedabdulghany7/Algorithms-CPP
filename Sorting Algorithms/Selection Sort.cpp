b4789e54fc0f3cf78dd517ab98df63e7400ef7c4
Ahmed Abd Elghani <30578096+ahmedabdulghany7@users.noreply.github.com>
2024-08-14 20:40:05 +0300

    Update Selection Sort.cpp

--- Sorting Algorithms/Selection Sort.cpp
+++ Sorting Algorithms/Selection Sort.cpp
@@ -1,1 +1,36 @@
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
 
+
+
+
+/*
+#include <bits/stdc++.h>
+#define ll long long
+#define FAST ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int size = 10000;
+int arr[size];
+using namespace std;
+
+int main() {
+    FAST
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n-1; i++) {
+        int min_index = i;
+        for(int j = i+1;j<n;j++) {
+            if(arr[min_index]>arr[j]) {
+                min_index = j;
+            }
+        }
+        swap(arr[i], arr[min_index]);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
+*/
