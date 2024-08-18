c2fe132bbe0c9018cda84dd910ed2a25c806932a
Ahmed Abd Elghani <30578096+ahmedabdulghany7@users.noreply.github.com>
2024-08-18 08:24:38 +0300

    Create Count Sort with negative values.cpp

---
+++ Sorting Algorithms/Count Sort with negative values.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define FAST ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int size = 1000;
+int arr[size];
+using namespace std;
+
+void count_sort(int arr[], int n) {
+    int mn = arr[0], mx = arr[0];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > mx) mx = arr[i];
+        if (arr[i] < mn) mn = arr[i];
+    }
+    int range = mx - mn + 1;
+    int freq[range] = {0};
+    for (int i = 0; i < n; i++) {
+        freq[arr[i] - mn]++;
+    }
+    int idx = 0;
+    for (int i = 0; i < range; i++) {
+        while (freq[i]--) {
+            arr[idx++] = i + mn;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+}
+
+int main() {
+    FAST
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    count_sort(arr, n);
+    return 0;
+}
