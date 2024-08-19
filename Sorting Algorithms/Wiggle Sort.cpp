e821ec7e5be7fff6538d0e5663ebb5722157d404
Ahmed Abd Elghani <30578096+ahmedabdulghany7@users.noreply.github.com>
2024-08-20 00:18:22 +0300

    Update Wiggle Sort.cpp

--- Sorting Algorithms/Wiggle Sort.cpp
+++ Sorting Algorithms/Wiggle Sort.cpp
@@ -1,1 +1,36 @@
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+
 //nums[0] <= nums[1] >= nums[2] <= nums[3]
+
+#include <bits/stdc++.h>
+#define FAST ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+void wiggleSort(vector<int> &nums) {
+    ranges::sort(nums);
+    int n = nums.size();
+    for (int i = 1; i + 1 < n; i += 2) {
+        swap(nums[i], nums[i + 1]);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << ' ';
+    }
+}
+
+vector<int> read_vector() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+    return v;
+}
+
+int main() {
+    FAST;
+    vector<int> v = read_vector();
+    wiggleSort(v);
+    return 0;
+}
+
